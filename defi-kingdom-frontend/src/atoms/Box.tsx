7ec77df46c89cf463675a965f36378566e0924aa
Ashish Chikane <ashishc0046@gmail.com>
2025-02-20 14:36:53 +0530

    url change

--- defi-kingdom-frontend/src/atoms/Box.tsx
+++ defi-kingdom-frontend/src/atoms/Box.tsx
@@ -10,6 +10,7 @@ const Box: React.FC<BoxProps> = ({ children, overflow = "" }) => {
     <div
       className={`bg-white ${overflow} dark:bg-gray-900 rounded-md border dark:border-gray-800 border-gray-300 mb-4 shadow-md p-6`}
     >
+      
       {children}
     </div>
   );
